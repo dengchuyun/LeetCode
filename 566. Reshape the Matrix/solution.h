9b58e2f237799a2867bbdb7a8b17adf49fec98a3
dengchuyun <dengchuyun@hotmail.com>
2017-08-23 20:21:23 +0800

    version 4 of 556

--- 566. Reshape the Matrix/solution.h
+++ 566. Reshape the Matrix/solution.h
@@ -8,13 +8,14 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
         size_t old_row = nums.size(), old_col = nums[0].size();
-        size_t elem_num = old_row * old_col;
+        size_t count = 0;
         vector<vector<int>> new_nums((size_t)r);
 
         if (old_row * old_col != r * c) return nums;
 
-        for (int i = 0; i < elem_num; ++i)
-            new_nums[i/c].push_back(nums[i/old_col][i%old_col]);
+        for (int i = 0; i < old_row; ++i)
+            for (int j = 0; j < old_col; ++j)
+                new_nums[count++/c].push_back(nums[i][j]);
 
         return new_nums;
     }
