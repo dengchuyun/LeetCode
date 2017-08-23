6a0bac157700e8de9f8314c9914a799563c39daa
dengchuyun <dengchuyun@hotmail.com>
2017-08-23 17:36:37 +0800

    version1 of 566

--- 566. Reshape the Matrix/main.cpp
+++ 566. Reshape the Matrix/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include "solution.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<vector<int>> nums = vector<vector<int>>(2);
+    vector<vector<int>> new_nums;
+    Solution solution = Solution();
+
+    for (int i = 0; i < 2; ++i)
+        nums[i] = vector<int>(2);
+    nums[0][0] = 1;
+    nums[0][1] = 2;
+    nums[1][0] = 3;
+    nums[1][1] = 4;
+
+    new_nums =  solution.matrixReshape(nums, 1, 4);
+
+    for (int i = 0; i < 4; ++i)
+        cout << new_nums[0][i] << endl;
+
     return 0;
 }
--- 566. Reshape the Matrix/solution.h
+++ 566. Reshape the Matrix/solution.h
@@ -7,7 +7,24 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+        size_t old_i = 0, old_j = 0;
+        size_t old_row = nums.size(), old_col = nums[0].size();
+        vector<vector<int>> new_nums = vector<vector<int>>((size_t)r);
 
+        if (old_row * old_col != r * c) return nums;
+
+        for (int i = 0; i < r; ++i) {
+            new_nums[i] = vector<int>((size_t)c);
+            for (int j = 0; j < c; ++j) {
+                if (old_j >= old_col){
+                    ++old_i;
+                    old_j = 0;
+                }
+                new_nums[i][j] = nums[old_i][old_j++];
+            }
+        }
+
+        return new_nums;
     }
 };
 
