5576cbd7765ee69ff60583eff4a20dbfb1255f4c
dengchuyun <dengchuyun@hotmail.com>
2018-01-16 23:05:09 +0800

    去除冗余文件

--- 561. Array Partition I/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "solution.h"
-
-int main() {
-    Solution solution;
-    vector<int> nums = {1, 4, 3, 2};
-    std::cout << solution.arrayPairSum(nums) << std::endl;
-    return 0;
-}
